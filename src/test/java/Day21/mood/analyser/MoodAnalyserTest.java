4565bd15b7f1f6705ea8e720efad05fc18f864f8
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-07 10:38:31 +0530

    Added : Through custom Exception for null mood

--- src/test/java/Day21/mood/analyser/MoodAnalyserTest.java
+++ src/test/java/Day21/mood/analyser/MoodAnalyserTest.java
@@ -7,15 +7,40 @@ import static org.junit.Assert.*;
 
 public class MoodAnalyserTest {
     MoodAnalyser moodAnalyser = new MoodAnalyser();
+
     @Test
-    public void givenMessage_IsProper_ShouldReturnSad() {
-        String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
+    public void givenMessage_IsProper_ShouldReturnSad() throws MoodAnalyserException {
+        moodAnalyser.setMessage("I am in Sad Mood");
+        String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", actualResult);
     }
 
     @Test
-    public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
-        String actualResult = moodAnalyser.analyseMood("I am in Any Mood");
+    public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() throws MoodAnalyserException {
+        moodAnalyser.setMessage("I am in Any Mood");
+        String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", actualResult);
     }
+
+    @Test
+    public void givenMessage_Null_ShouldThrowMoodAnalysisException() {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.exceptionType);
+        }
+    }
+
+    @Test
+    public void givenMessage_Empty_ShouldThrowMoodAnalysisException() {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("");
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.exceptionType);
+        }
+    }
 }
+
+    
