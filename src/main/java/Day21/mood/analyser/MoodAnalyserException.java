02452bf198a056a34f8c2019af1b31c5a89cd113
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-07 11:15:37 +0530

    Added : Through Custom Exception if user entr invalid mood

---
+++ src/main/java/Day21/mood/analyser/MoodAnalyserException.java
@@ -0,0 +1,9 @@
+package Day21.mood.analyser;
+
+public class MoodAnalyserException extends Exception {
+    public String message;
+
+    public MoodAnalyserException(String message) {
+        this.message = message;
+    }
+}
